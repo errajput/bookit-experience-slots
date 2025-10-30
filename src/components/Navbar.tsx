aa07b0a2a11a1cc56efa24d8d75007fa071ff892
disha rajput <dishacsevira@gmail.com>
2025-10-30 21:31:58 +0530

    Add search in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,33 @@
 "use client";
-import { UserContext } from "@/providers";
 import Image from "next/image";
-import { useContext } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
-  const { search, setSearch } = useContext(UserContext);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const q = searchParams.get("q");
+  const [searchTerm, setSearchTerm] = useState(q || "");
+
+  useEffect(() => {
+    if (pathname) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setSearchTerm(q || "");
+    }
+  }, [pathname, q]);
+
+  const handleSearch = () => {
+    const params = new URLSearchParams(searchParams);
+    if (searchTerm) {
+      params.set("q", searchTerm);
+    } else {
+      params.delete("q"); // Remove if search term is empty
+    }
+    router.push(`/?${params.toString()}`);
+  };
+
   return (
     <header className=" bg-white shadow-[0_2px_16px_rgba(0,0,0,0.1)]">
       <div className="max-w-6xl mx-auto flex items-center justify-between py-4 px-4">
@@ -20,10 +43,13 @@ export default function Navbar() {
             placeholder="Search experiences"
             className="bg-[#EDEDED] text-[#727272] rounded px-3 py-2 text-sm w-64"
             id="global-search"
-            value={search}
-            onChange={(e) => setSearch?.(e.target.value)}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <button className="bg-brand px-3 py-2 rounded-lg text-[#161616] bg-[#FFD643] font-medium cursor-pointer">
+          <button
+            className="bg-brand px-3 py-2 rounded-lg text-[#161616] bg-[#FFD643] font-medium cursor-pointer"
+            onClick={handleSearch}
+          >
             Search
           </button>
         </div>
