3fc2b1be99eb76b5812ad1911f083a6a92885084
disha rajput <dishacsevira@gmail.com>
2025-10-31 12:12:46 +0530

    change in header style, home page style

--- src/components/ExeprienceCard.tsx
+++ src/components/ExeprienceCard.tsx
@@ -10,9 +10,9 @@ export default function ExperienceCard({
   experience: Experience;
 }) {
   return (
-    <div className="bg-[#F0F0F0] rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300">
+    <div className="bg-[#F0F0F0] w-[270px] h-[312px] rounded-xl overflow-hidden shadow-sm hover:shadow-md transition-all duration-300 border border-gray-100">
       {/* Image Section */}
-      <div className="relative w-full h-56">
+      <div className="relative w-full h-[180px]">
         {experience.image ? (
           <Image
             src={experience.image}
@@ -21,38 +21,38 @@ export default function ExperienceCard({
             className="object-cover"
           />
         ) : (
-          <div className="h-56 bg-gray-200" />
+          <div className="h-[180px] bg-gray-200" />
         )}
       </div>
 
       {/* Content */}
-      <div className="p-5">
+      <div className="p-3">
         <div className="flex items-start justify-between">
-          <h3 className="text-lg font-semibold text-gray-900">
+          <h3 className="font-inter font-medium text-gray-900 text-[16px] leading-tight">
             {experience.title}
           </h3>
           {experience.location && (
-            <span className="text-sm bg-[#D6D6D6] text-gray-700 px-3 py-1 rounded-md whitespace-nowrap">
+            <span className=" bg-[#D6D6D6] text-[#161616] font-inter font-medium text-[11px] px-2 py-0.5 rounded whitespace-nowrap ml-2">
               {experience.location}
             </span>
           )}
         </div>
 
-        <p className="text-sm text-gray-500 mt-2 leading-relaxed">
+        <p className="text-[12px] w-[248px] h-8 text-[#6C6C6C] mt-2 leading-relaxed object-cover">
           Curated small-group experience. Certified guide. Safety first with
           gear included.
         </p>
 
-        <div className="flex items-center justify-between mt-5">
-          <div className="text-sm text-gray-600">
+        <div className="flex items-center justify-between mt-4">
+          <div className=" flex items-center justify-between gap-2 text-[13px] text-black">
             From{" "}
-            <span className="font-bold text-black text-lg">
+            <span className=" text-black font-inter font-semibold text-[20px]">
               ₹{experience.price}
             </span>
           </div>
 
           <Link href={`/experiences/${experience._id}`}>
-            <button className="bg-yellow-400 hover:bg-yellow-300 text-gray-900 font-medium px-5 py-2 rounded-md transition-colors duration-200 cursor-pointer">
+            <button className="cursor-pointer bg-[#FFD643] hover:bg-[#FFE273] text-gray-900 font-inter font-medium text-[14px] px-3 py-1.5 rounded transition-all duration-200">
               View Details
             </button>
           </Link>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,8 +29,8 @@ export default function Navbar() {
   };
 
   return (
-    <header className=" bg-white shadow-[0_2px_16px_rgba(0,0,0,0.1)]">
-      <div className="max-w-6xl mx-auto flex items-center justify-between py-4 px-4">
+    <header className=" bg-white shadow-[0_2px_16px_rgba(0,0,0,0.1)]  h-[87px]">
+      <div className="max-w-6xl mx-auto flex items-center justify-between py-4 px-3">
         <Image
           src={"/HDlogo-1.png"}
           alt={"highway delite"}
@@ -41,13 +41,13 @@ export default function Navbar() {
         <div className="flex items-center gap-3">
           <input
             placeholder="Search experiences"
-            className="bg-[#EDEDED] text-[#727272] rounded px-3 py-2 text-sm w-64"
+            className="bg-[#EDEDED] text-[#727272] rounded px-3 py-2 text-[14px] w-[340px]"
             id="global-search"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button
-            className="bg-brand px-3 py-2 rounded-lg text-[#161616] bg-[#FFD643] font-medium cursor-pointer"
+            className="bg-brand w-[87px] h-[42px] px-3 py-2 rounded-lg text-[#161616] bg-[#FFD643] text-[14px] font-medium cursor-pointer"
             onClick={handleSearch}
           >
             Search
